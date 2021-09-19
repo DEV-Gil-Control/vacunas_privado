ec0395cc1447793997d7ed845457c56c1520ff86
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-19 10:43:34 -0500

    Update user.js

--- user.js
+++ user.js
@@ -34,7 +34,7 @@ var push_to_firebase = function(data){
         var db = firebase.firestore();
 
         db.collection("messages").add({
-            //email: data["email"],
+            email: data["email"],
             namemenor: data["namemenor"],
            edadmenor: data["edadmenor"],
             apaternomenor: data["apaternomenor"],
@@ -62,7 +62,7 @@ var push_to_firebase = function(data){
       }
 
       var contact_submit = function(){
-          // var email = document.getElementById("email");
+          var email = document.getElementById("email");
         var namemenor = document.getElementById("namemenor");
          var edadmenor = document.getElementById("edademenor");
         var apaternomenor = document.getElementById("apaternomenor");
@@ -80,7 +80,7 @@ var push_to_firebase = function(data){
           var redtutor = document.getElementById("redtutor");
 
         var data = {
-            //"email": email.value,
+            "email": email.value,
           "namemenor": namemenor.value,
            "edadmenor": edadmenor.value,
           "apaternomenor": apaternomenor.value,

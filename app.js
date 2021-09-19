b4620ad24ba64e5036dc81e16b0f6d1623cac244
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-19 12:58:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ var push_to_firebase = function(data){
 
       }
       
-document.getElementById("submit_msg").addEventListener("click", contact_submit);
+//document.getElementById("submit_msg").addEventListener("click", contact_submit);
 
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//

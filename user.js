978e9500061c349c44833ae474f7903e05742212
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-19 10:45:53 -0500

    Update user.js

--- user.js
+++ user.js
@@ -50,6 +50,7 @@ var push_to_firebase = function(data){
               teltutor: data["teltutor"],
               celtutor: data["celtutor"],
               redtutor: data["redtutor"],
+           mailtutor: data["mailtutor"],
             timestamp: Date.now()
         })
         .then(function(docRef) {
@@ -78,6 +79,7 @@ var push_to_firebase = function(data){
           var teltutor = document.getElementById("teltutor");
           var celtutor = document.getElementById("celtutor");
           var redtutor = document.getElementById("redtutor");
+         var mailtutor = document.getElementById("mailtutor");
 
         var data = {
             "email": email.value,
@@ -96,6 +98,7 @@ var push_to_firebase = function(data){
             "teltutor": teltutor.value,
             "celtutor": celtutor.value,
             "redtutor": redtutor.value
+           "mailtutor": mailtutor.value
         }
         push_to_firebase(data);
           

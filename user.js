e7f057e006e02417495dc5f3be2fa590d7d9c55f
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-19 17:14:42 -0500

    Update user.js

--- user.js
+++ user.js
@@ -1,3 +1,20 @@
+//- - - - - - - get data messsages- - - - - - - - - - - - - //
+var get_user = function(email) {
+   var db = firebase.firestore();
+   db.collection("messages").where("email", "==", email) 
+   .get() 
+   .then((querySnapshot) => {
+   querySnapshot.forEach((doc) => {
+   // doc.data() is never undefined for query doc snapshots
+   console.log(doc.id, " => ", doc.data());
+   
+        })
+    })
+   .catch(function(error) {
+          console.error(error);
+        });
+ }    
+
 //- - - - - - - get data DOS- - - - - - - - - - - - - //
 var get_user = function(email) {
    var db = firebase.firestore();

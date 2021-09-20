0b2ada02583f9e6420bb92c47cf090e60d862efa
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-19 21:55:28 -0500

    Update user.js

--- user.js
+++ user.js
@@ -19,6 +19,16 @@ var get_user = function(email) {
         });
  }
 
+async getMarkers() {
+  const events = await firebase.firestore().collection('messages')
+  events.get().then((querySnapshot) => {
+      const tempDoc = []
+      querySnapshot.forEach((doc) => {
+         tempDoc.push({ id: doc.id, ...doc.data() })
+      })
+      console.log(tempDoc)
+   })
+ }
 
 //- - - - - - - - -  push data - - - - - - - - - - - -//
 

c93d659ab6004b69812042653deb43ca911743b3
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-19 21:50:34 -0500

    Update user.js

--- user.js
+++ user.js
@@ -7,14 +7,13 @@ var get_user = function(email) {
    querySnapshot.forEach((doc) => {
    // doc.data() is never undefined for query doc snapshots
    console.log(doc.id, " => ", doc.data());
-
-ref.on("messages", function(snapshot) {
-    var childData = snapshot.val();
-    var key = Object.keys(childData)[2];    //this will return 1st key.         
-    console.log(childData[key].id);
-});
+       var menor = document.getElementById("menor");
+       menor.innerHTML = `<div>Menor registrado: ${doc.data().namemenor} ${doc.data().apaternomenor} ${doc.data().amaternomenor}</div>
+       <div>CURP: ${doc.data().curpmenor}</div>
+       <div>Tutor registrado: ${doc.data().nametutor} ${doc.data().apaternotutor} ${doc.data().amaternotutor}</div>
+       <div>Correo Electrónico: ${doc.data().mailtutor}</div>`;
         })
-   }
+    })
    .catch(function(error) {
           console.error(error);
         });

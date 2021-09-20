a3313f21bd9c42dcc5f83c078eecffeef67770d1
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-20 12:40:28 -0500

    Update user.js

--- user.js
+++ user.js
@@ -15,7 +15,7 @@ var get_user = function(email) {
        <div>Correo Electrónico: ${doc.data().mailtutor}</div>`;
        })
       var menord = document.getElementById("menord"); 
-      enord.innerHTML = `<div>ID: ${doc.id}</div>
+      menord.innerHTML = `<div>ID: ${doc.id}</div>
        <div>Menor registrado: ${doc.data().namemenor} ${doc.data().apaternomenor} ${doc.data().amaternomenor}</div>
        <div>CURP: ${doc.data().curpmenor}</div>
        <div>Tutor registrado: ${doc.data().nametutor} ${doc.data().apaternotutor} ${doc.data().amaternotutor}</div>

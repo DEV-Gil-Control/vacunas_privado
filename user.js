d63f0ca14d0a3f9105fc010d944097dd49202207
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-19 23:20:02 -0500

    Update user.js

--- user.js
+++ user.js
@@ -1,24 +1,3 @@
-//- - - - - - - get data- - - - - - - - - - - - //
-function getdata() {
-    var user=document.getElementById("messages").value;
-    //firebase data retrieval function
-    //path of your data
-    //.once will get all your data in one time
-    firebase.database().ref('messages').once('value').then(function (snapshot) {
-        //here we will get data
-        //enter your field name
-        var name=snapshot.val().namemenor;
-        var gender=snapshot.val().edadmenor;
-        var country=snapshot.val().curpmenor;
-
-        //now we have data in variables
-        //now show them in our html
-
-        document.getElementById("namemenor").innerHTML=nombre;
-        document.getElementById("edadmenor").innerHTML=edad;
-        document.getElementById("curpmenor").innerHTML=curp;
-    })
-}
 
 
 //- - - - - - - - -  push data - - - - - - - - - - - -//
@@ -118,6 +97,30 @@ function login() {
     console.log(errorCode , " -" + errorMessage)
   });email-password.html
 }
+
+
+//- - - - - - - get data- - - - - - - - - - - - //
+function getdata() {
+    var user=document.getElementById("messages").value;
+    //firebase data retrieval function
+    //path of your data
+    //.once will get all your data in one time
+    firebase.database().ref('messages').once('value').then(function (snapshot) {
+        //here we will get data
+        //enter your field name
+        var name=snapshot.val().namemenor;
+        var gender=snapshot.val().edadmenor;
+        var country=snapshot.val().curpmenor;
+
+        //now we have data in variables
+        //now show them in our html
+
+        document.getElementById("namemenor").innerHTML=nombre;
+        document.getElementById("edadmenor").innerHTML=edad;
+        document.getElementById("curpmenor").innerHTML=curp;
+    })
+}
+//- - - -  - - - - - - - - - - - - //
 
 function observer(){
   firebase.auth().onAuthStateChanged(function(user) {

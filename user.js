4543d7e363fcca644407f4cba63c80f2693afce9
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-20 13:03:30 -0500

    Update user.js

--- user.js
+++ user.js
@@ -13,8 +13,8 @@ var get_user = function(email) {
        <div>CURP: ${doc.data().curpmenor}</div>
        <div>Tutor registrado: ${doc.data().nametutor} ${doc.data().apaternotutor} ${doc.data().amaternotutor}</div>
        <div>Correo Electrónico: ${doc.data().mailtutor}</div>`;
-       })
-
+               })
+    })
    .catch(function(error) {
           console.error(error);
         });

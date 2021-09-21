1245e8e557d9f91393d244a9f25846ae4de7caec
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-20 23:38:42 -0500

    Update legal.js

--- legal.js
+++ legal.js
@@ -1,3 +1,18 @@
+//- - - - - - - LEER QUERY - - - - - - - - - - //
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//
 
 //- - - - - - - get data- - - - - - - - - - - - - //
 var get_user = function(email) {
@@ -44,6 +59,7 @@ var get_user = function(email) {
 
   
 
+    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -//
 
 //- - - - - - - - -  push data - - - - - - - - - - - -//
 

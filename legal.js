3d99a6c26546b07c9d2236f3a3c30e84cc88d193
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-21 12:00:14 -0500

    Update legal.js

--- legal.js
+++ legal.js
@@ -13,7 +13,7 @@ var get_user = function(email) {
    .then((querySnapshot) => {
    querySnapshot.forEach((doc) => {
     //doc.data() is never undefined for query doc snapshots
-     console.log(doc.id, " => ", doc.data());
+     console.log(document.urlParams.getAll('ID');, " => ", doc.data());
        var todosd = document.getElementById("todosd");
        //var todosd = document.urlParams.getAll('ID'); ("todosd");
        todosd.innerHTML = `<div><strong>Nombre del padre, madre o tutor:</strong> ${doc.data().nametutor} ${doc.data().apaternotutor} ${doc.data().amaternotutor}</div>

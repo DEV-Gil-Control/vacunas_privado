dcae44effcbd8b4fe7e847b1ddbf6522df1c1644
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-21 00:10:04 -0500

    Update legal.js

--- legal.js
+++ legal.js
@@ -13,7 +13,7 @@ var get_user = function(email) {
    db.collection("messages").where("email", "==", email) 
    .get() 
    .then((querySnapshot) => {
-   querySnapshot.forEach((doc) => {
+   querySnapshot.urlParams((doc) => {
    // doc.data() is never undefined for query doc snapshots
    console.log(doc.id, " => ", doc.data());
        var todosd = document.getElementById("todosd");

f8e403acb56481490ea7c0e0c41655bff1c8aca2
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-21 16:53:06 -0500

    Update user.js

--- user.js
+++ user.js
@@ -10,8 +10,8 @@ var get_user = function(email) {
        var menor = document.getElementById("menor");
        menor.innerHTML = menor.innerHTML + `<div>ID: ${doc.id}</div>
        <div style="padding-top:20px;">
-       <div><strong>Menor registrado:</strong><a href="#" class="text-decoration-none">Non-underlined link</a></div>
-       <div>Nombre del Niño/Niña: ${doc.data().namemenor}</div>
+       <div><strong>Menor registrado:</strong></div>
+       <div>Nombre del Niño/Niña: ${doc.data().namemenor} <a href="#" class="text-decoration-none">EDITAR</a></div>
        <div>Apellido paterno: ${doc.data().apaternomenor}</div>
        <div>Apellido Materno: ${doc.data().amaternomenor}</div>
        <div>Edad: ${doc.data().edadmenor}</div>

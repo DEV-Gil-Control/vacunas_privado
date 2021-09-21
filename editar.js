28bea9f27f10ad9f49f3ba5fece2076a5cb26294
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-21 18:11:13 -0500

    Update editar.js

--- editar.js
+++ editar.js
@@ -25,7 +25,7 @@ var get_user = function(email) {
        <div><strong>Menor registrado:</strong></div>
        <div>Nombre del Niño/Niña: ${doc.data().namemenor}
        <label for="namemenor"></label>
-       <input type="text" id="namemenor" name="namemenor" placeholder="Cambiar Nombre completo" style="width: 50%; margin-left:10px;">
+       <input type="text" id="namemenor" name="namemenor" placeholder="NUEVO Nombre de Niño/Niña" style="width: 50%; margin-top:-5px;">
        </div>
        <div>Apellido paterno: ${doc.data().apaternomenor}</div>
        <div>Apellido Materno: ${doc.data().amaternomenor}</div>

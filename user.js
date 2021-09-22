71224d4b892004f5caa5a38e16a10e65f38e24fd
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-21 23:44:55 -0500

    Update user.js

--- user.js
+++ user.js
@@ -28,7 +28,7 @@ var get_user = function(email) {
        <div>Celular: ${doc.data().celtutor}</div>
        <div>Correo Electrónico: ${doc.data().mailtutor}</div>
        <div>Red social: ${doc.data().redtutor}</div>
-       <div style="padding-top:20px;">
+       <div style="padding-top:20px; text-align:right;">
        <input type="button" class="btn btn-info" value="Editar o Cambiar Campos" onclick="window.location.replace('https://private.vacunas.chaledelafuente.mx/editar.html?ID=${doc.id}')" />
        <input type="button" class="btn btn-dark" value="Ver e imprimir el aviso de privacidad" onclick="window.open('https://private.vacunas.chaledelafuente.mx/legal.html?ID=${doc.id}')" />
        <button type="button" class="btn btn-success">Ver e imprimir su amparo</button>

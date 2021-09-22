37ad7ab19a0a25d0a0d001edf6c9b3737b9bafdc
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-22 18:43:29 -0500

    Update user.js

--- user.js
+++ user.js
@@ -259,7 +259,7 @@ function show(user) {
     content.innerHTML = `
       <div class="container mt-5">
         <div class="card">
-          <h5 class="card-header">Bienvenido, gracias participar en el Amparo Colectivo: Vacunas COVID-19 para menores de edad</h5>
+          <h5 class="card-header">Bienvenido al sistema de alta y seguimiento para el amparo colectivo para la vacunación COVID-9 a menores</h5>
             <div id="bientutor" style="margin-left:22px; margin-top:20px; font-weight: bold;">
             </div>
           <div class="card-body">
@@ -277,7 +277,7 @@ function show(user) {
     content.innerHTML = `
       <div class="container mt-5">
         <div class="card">
-          <h5 class="card-header">Bienvenido, gracias participar en el Amparo Colectivo: Vacunas COVID-19 para menores de edad</h5>
+          <h5 class="card-header">Bienvenido al sistema de alta y seguimiento para el amparo colectivo para la vacunación COVID-9 a menores</h5>
             <div id="bientutor" style="margin-left:22px; margin-top:20px; font-weight: bold;">
             </div>
           <div class="card-body">

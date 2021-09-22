9b6216b253a3c7519f1f155bfe51372fd1cb5b39
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-21 23:37:21 -0500

    Update user.js

--- user.js
+++ user.js
@@ -8,7 +8,7 @@ var get_user = function(email) {
    // doc.data() is never undefined for query doc snapshots
    console.log(doc.id, " => ", doc.data());
        var menor = document.getElementById("menor");
-       menor.innerHTML = menor.innerHTML + `<div>ID: ${doc.id}</div>
+       menor.innerHTML = menor.innerHTML + `<div class="alert alert-dark" role="alert">ID: ${doc.id}</div>
        <div style="padding-top:20px;">
        <div class="alert alert-primary" role="alert"><strong>Menor registrado:</strong></div>
        <div>Nombre del Niño/Niña: ${doc.data().namemenor}</div>

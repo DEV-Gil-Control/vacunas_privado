a5cdcc585d67c590aa8faab9363bdf77ff21d363
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-23 11:31:04 -0500

    Update supervisor.js

--- supervisor.js
+++ supervisor.js
@@ -8,7 +8,7 @@ var get_user = function(email) {
    // doc.data() is never undefined for query doc snapshots
    console.log(doc.id, " => ", doc.data());
       
-       var menor = document.getElementById.were("email", "==", "gilhernandez@gmail.com")("menor");
+       var menor = document.getElementById("menor");
        menor.innerHTML = menor.innerHTML + `<div class="alert alert-info" role="alert"><strong>Registrdo por:</strong> ${doc.data().email}</div>
        <div class="alert alert-dark" role="alert" style="margin-top:-20px;"><strong>CURP:</strong> ${doc.data().curpmenor}</div>
        <div class="alert alert-primary" role="alert" style="margin-top:-20px;"><strong>Menor registrado:</strong></div>

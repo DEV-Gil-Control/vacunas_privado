581bd56790914da83710afc8a9dab2ca2aecb4af
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-23 12:11:45 -0500

    Update menusupervisor.js

--- menusupervisor.js
+++ menusupervisor.js
@@ -6,7 +6,7 @@ var get_user = function(email) {
    .then((querySnapshot) => {
    querySnapshot.forEach((doc) => {
    // doc.data() is never undefined for query doc snapshots
-   console.log(doc.id, " = ", doc.data());
+   console.log(doc.id, " = ", doc.data().email);
        var menor = document.getElementById("menor");
        menor.innerHTML = `<div class="alert alert-dark" role="alert"><strong>Administrador:</strong> ${doc.data().email}</div>
        `;

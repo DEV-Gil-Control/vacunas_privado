d6431744b1d34dac2128608ef4c122016d48e505
DEV-Gil-Control <90582410+DEV-Gil-Control@users.noreply.github.com>
2021-09-23 11:25:00 -0500

    Update supervisor.js

--- supervisor.js
+++ supervisor.js
@@ -46,50 +46,6 @@ var get_user = function(email) {
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 //- - - - - - - get dos- - - - - - - - - - - - //
-var get_user = function(email) {
-   var db = firebase.firestore();
-   db.collection("messages").where("email", "==", "reto_80@hotmail.com")
-   .get()
-   .then((querySnapshot) => {
-   querySnapshot.forEach((doc) => {
-   // doc.data() is never undefined for query doc snapshots
-   console.log(doc.id, " => ", doc.data());
-      
-       var menordos = document.getElementById("menordos");
-       menordos.innerHTML = menordos.innerHTML + `<div class="alert alert-info" role="alert"><strong>Registrdo por:</strong> ${doc.data().email}</div>
-       <div class="alert alert-dark" role="alert" style="margin-top:-20px;"><strong>CURP:</strong> ${doc.data().curpmenor}</div>
-       <div class="alert alert-primary" role="alert" style="margin-top:-20px;"><strong>Menor registrado:</strong></div>
-       <div>Nombre del Niño/Niña: ${doc.data().namemenor}</div>
-       <div>Apellido paterno: ${doc.data().apaternomenor}</div>
-       <div>Apellido Materno: ${doc.data().amaternomenor}</div>
-       <div>Edad: ${doc.data().edadmenor}</div>
-       <div>Ciudad de Nacimiento: ${doc.data().ciudadmenor}</div>
-       <div>Estado de Nacimiento: ${doc.data().estadomenor}</div>
-       <div>CURP: ${doc.data().curpmenor}</div>
-       <div style="padding-top:20px;">
-       <div class="alert alert-primary" role="alert"><strong>Tutor responsable que lo registra:</strong></div>
-       <div>Nombre: ${doc.data().nametutor}</div>
-       <div>Apellido paterno: ${doc.data().apaternotutor}</div>
-       <div>Apellido materno: ${doc.data().amaternotutor}</div>
-       <div>Domicilio: ${doc.data().domiciliotutor}</div>
-       <div>Colonia: ${doc.data().teltutor}</div>
-       <div>Código Postal: ${doc.data().cptutor}</div>
-       <div>Municipio: ${doc.data().mpiotutor}</div>
-       <div>Tel. casa u oficna: ${doc.data().teltutor}</div>
-       <div>Celular: ${doc.data().celtutor}</div>
-       <div>Correo Electrónico: ${doc.data().mailtutor}</div>
-       <div>Red social: ${doc.data().redtutor}</div>
-       <div style="padding-top:20px;">
-       <div class="alert alert-warning" role="alert"><strong>Estatus:</strong></div>
-       <div><strong>Aviso de Privacidad Impreso:</strong> ${doc.data().privacidad}</div>
-       <div><strong>Amparo Impreso:</strong> ${doc.data().amparo}</div>
-       <div style="margin-bottom:30px;"></div>`;
-               })
-    })
-   .catch(function(error) {
-          console.error(error);
-        });
- }  
 
 
 //- - - - - - - - -  Query String - - - - - - - - - - - -//
